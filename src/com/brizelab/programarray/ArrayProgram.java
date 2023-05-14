e57d964b92a94adfbf761fabb3c6b2e32e576830
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-14 16:23:07 +0530

    Added:print element asending order

--- src/com/brizelab/programarray/ArrayProgram.java
+++ src/com/brizelab/programarray/ArrayProgram.java
@@ -77,6 +77,18 @@ public class ArrayProgram {
 
         }
     }
+    public static void sortArray(int arr[]){
+        int temp;
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = i+1; j <arr.length ; j++) {
+                if(arr[j]<arr[i]) {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+
+        }
 
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
@@ -95,5 +107,6 @@ public class ArrayProgram {
             oddPosition(arr);
             reverseOrder(arr);
             printDublicate(arr);
+            sortArray(arr);
     }
 }
