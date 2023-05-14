18802428e8b60cb54460c3c1a35c510ee9361636
Alok kumar prajapati <alokprajapati913@gmail.com>
2023-05-14 16:27:16 +0530

    Added:find second largest element program

--- src/com/brizelab/programarray/ArrayProgram.java
+++ src/com/brizelab/programarray/ArrayProgram.java
@@ -89,6 +89,23 @@ public class ArrayProgram {
             }
 
         }
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]+" ");
+
+        }
+    }public static void secondLargest(int arr[]) {
+        int temp;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("second largest:" + arr[arr.length - 2]);
+    }
 
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
@@ -108,5 +125,6 @@ public class ArrayProgram {
             reverseOrder(arr);
             printDublicate(arr);
             sortArray(arr);
+            secondLargest(arr);
     }
 }
